5a7f9a9ed7f249eac8216cf68be877fad0e58264
Muhammed Rhasil <muhammed.rhasil7@gmail.com>
2025-08-23 13:23:00 +0530

    refactor: Use asyncWrap for cleaner error handling

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,57 +1,44 @@
 const Task = require("../models/tasks");
-const createTask = async (req,res)=>{
-    try {
-        const task = new Task(req.body);
-        const savedTask = await task.save();
-        res.status(201).json(savedTask);
-    } catch (error) {
-       res.status(400).json({success: false, message: error.message}) 
+const asyncWrap = require("../middleware/async");
+const {createCustomError} = require("../errors/error");
+
+//Controller for creating task
+const createTask = asyncWrap(async (req,res)=>{
+    const task = new Task(req.body);
+    const savedTask = await task.save();
+    res.status(201).json(savedTask);
+})
+//Controller for updating task
+const updateTask = asyncWrap(async (req, res)=>{
+    const {id} = req.params;
+    const task = await Task.findByIdAndUpdate(id,req.body,{new: true, runValidators: true});
+    if(!task){
+        throw new createCustomError(`No Task found with id: ${id}`,404);
     }
-}
-const updateTask = async (req, res)=>{
-    try {
-        const {id} = req.params;
-        const task = await Task.findByIdAndUpdate(id,req.body,{new: true, runValidators: true});
-        if(!task){
-            return res.status(404).json({message: `File not found at id: ${id}`});
-        }
-        res.status(200).json({ task });
-    } catch (error) {
-        res.status(500).json({success:false, message: error.message})
+    res.status(200).json({ task });
+})
+//Controller for deleting tasks
+const deleteTask = asyncWrap(async (req,res)=>{
+    const {id} = req.params;
+    const task = await Task.findByIdAndDelete(id);
+    if(!task){
+        throw new createCustomError(`No task found with id: ${id}`,404);
     }
-}
-const deleteTask = async (req,res)=>{
-    try {
-        const {id} = req.params;
-        const task = await Task.findByIdAndDelete(id);
-         if(!task){
-            return res.status(404).json({message: `No task found at the id: ${id}`});
-        }
-        res.status(200).json({task});
-    } catch (error) {
-        res.status(500).json({success: false,message: error.message});
-    }
-}
+    res.status(200).json({task});
+});
+//Controller for retrieving all tasks
 const getAllTask = async (req,res)=>{
-    try {
-        const tasks = await Task.find();
-        res.status(200).json({tasks});
-    } catch (error) {
-        res.status(500).json({success: false, message: error.message});
-    }
+    const tasks = await Task.find();
+    res.status(200).json({tasks});
 }
+//Controller for retrieving specific task
 const getTask = async (req,res)=>{
     const {id} = req.params;
-    console.log(id);
-    try {
-        const task = await Task.findById(id);
-        if(!task){
-            return res.status(404).json({msg: `No task with id: ${id}`});
-        }
-        res.status(200).json({task});
-    } catch (error) {
-        res.status(500).json({success:false, message: error.message});
+    const task = await Task.findById(id);
+    if(!task){
+        throw new createCustomError(`No task found with id: ${id}`,404);
     }
+    res.status(200).json({task});
 }
 module.exports = {
     createTask,
