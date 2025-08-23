085403df52c42a4bf03ecd68ecef284b43d308e5
Muhammed Rhasil <muhammed.rhasil7@gmail.com>
2025-08-23 13:20:14 +0530

    feat: Implement custom error utility

---
+++ errors/error.js
@@ -0,0 +1,10 @@
+class customError extends Error{
+    constructor(message,statusCode){
+        super(message);
+        this.statusCode = statusCode;
+    }
+}
+const createCustomError = (msg, statusCode)=>{
+    return new customError(msg, statusCode);
+}
+module.exports = {customError, createCustomError};
