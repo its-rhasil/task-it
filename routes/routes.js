c07430da0a95125349b098cb0abb1751ec446bb3
Muhammed Rhasil <muhammed.rhasil7@gmail.com>
2025-08-23 13:27:50 +0530

    refactor: Restructure routes to handle CRUD operations

--- routes/routes.js
+++ routes/routes.js
@@ -3,8 +3,8 @@ const router = express.Router()
 const {createTask,
     updateTask,
     deleteTask,
+    getAllTask,
     getTask } = require("../controllers/tasks")
-router.route("/").get(getTask).post(createTask);
+router.route("/").get(getAllTask).post(createTask);
 router.route("/:id").get(getTask).patch(updateTask).delete(deleteTask);
-
 module.exports = router
